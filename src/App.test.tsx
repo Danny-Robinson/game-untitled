d35573f4db9e79441185345129740c56adf5e327
Danny <dannyrobinson94@hotmail.com>
2020-03-31 22:49:24 +0100

    second commit

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn MY DICK/i);
+  expect(linkElement).toBeInTheDocument();
+});
