854cf7abb38e21c6edf929fd76413f99248ca60d
Danny <dannyrobinson94@hotmail.com>
2020-04-09 13:28:03 +0100

    Removed console logging

--- src/redux-common/localstorage.ts
+++ src/redux-common/localstorage.ts
@@ -3,19 +3,16 @@ import { StoreState } from "./store";
 export const loadState = () => {
   try {
     const state = localStorage.getItem("state");
-    console.log("loading state");
-    console.log(state);
     if (!state) return undefined;
     return JSON.parse(state);
   } catch (error) {
+    window.alert("Enable browser local storage to allow auto-saving");
     return undefined;
   }
 };
 
 export const saveState = (state: StoreState) => {
   try {
-    console.log("saving");
-    console.log(JSON.stringify(state));
     localStorage.setItem("state", JSON.stringify(state));
   } catch (error) {
     return;
